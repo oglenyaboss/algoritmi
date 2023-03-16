f10a65d520b3ad9f28ccaa2e8b6dc18a0d918b1a
dog <dog@DOGB5AA>
2023-03-16 15:19:41 +0700

    Add project files.

---
+++ algoritmireal/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace algoritmireal
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            tabPage2 = new TabPage();
+            dataGridView2 = new DataGridView();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            tabPage3 = new TabPage();
+            label6 = new Label();
+            label5 = new Label();
+            button3 = new Button();
+            label4 = new Label();
+            tabPage4 = new TabPage();
+            button4 = new Button();
+            label8 = new Label();
+            label7 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            tabPage3.SuspendLayout();
+            tabPage4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Location = new Point(12, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(788, 438);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(780, 410);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(314, 317);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 4;
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(314, 285);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(330, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 2;
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(293, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(306, 182);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "vektori";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(dataGridView2);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(dataGridView1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(780, 410);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(393, 8);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.Size = new Size(379, 334);
+            dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(351, 363);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "matrici";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(20, 8);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(367, 334);
+            dataGridView1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            tabPage3.Controls.Add(label6);
+            tabPage3.Controls.Add(label5);
+            tabPage3.Controls.Add(button3);
+            tabPage3.Controls.Add(label4);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(780, 410);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            tabPage3.UseVisualStyleBackColor = true;
+            tabPage3.Click += tabPage3_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(342, 256);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 15);
+            label6.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(342, 241);
+            label5.Name = "label5";
+            label5.Size = new Size(0, 15);
+            label5.TabIndex = 2;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(325, 274);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 1;
+            button3.Text = "stroki";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(173, 157);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(button4);
+            tabPage4.Controls.Add(label8);
+            tabPage4.Controls.Add(label7);
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(780, 410);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "tabPage4";
+            tabPage4.UseVisualStyleBackColor = true;
+            tabPage4.Click += tabPage4_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(328, 294);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 2;
+            button4.Text = "tablici";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(52, 37);
+            label8.Name = "label8";
+            label8.Size = new Size(0, 15);
+            label8.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(52, 136);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 15);
+            label7.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            tabPage4.ResumeLayout(false);
+            tabPage4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TabPage tabPage2;
+        private Label label3;
+        private Label label2;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private TabPage tabPage3;
+        private Label label6;
+        private Label label5;
+        private Button button3;
+        private Label label4;
+        private TabPage tabPage4;
+        private Button button4;
+        private Label label8;
+        private Label label7;
+    }
+}
---
+++ algoritmireal/Form1.cs
@@ -0,0 +1,178 @@
+﻿namespace algoritmireal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int n = int.Parse(textBox1.Text);
+            double[] arr = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = rnd.Next(0, 5);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] < 2)
+                {
+                    arr[i] = 0;
+                }
+            }
+            double sum = 0;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] >= 3 && arr[i] <= 7)
+                {
+                    sum += arr[i];
+                    count++;
+                }
+            }
+            label1.Text = ($"Измененная последовательность: {string.Join(", ", arr)}");
+            label2.Text = ($"Сумма элементов от 3 до 7: {sum}");
+            label3.Text = ($"Количество элементов от 3 до 7: {count}");
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 12;
+            dataGridView1.ColumnCount = 12;
+            dataGridView2.RowCount = 12;
+            dataGridView2.ColumnCount = 12;
+            Random random = new Random();
+            double[,] matrix = new double[12, 12];
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                    dataGridView1.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = i; j < 12; j++)
+                {
+                    matrix[i, j] = 0;
+                    dataGridView2.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    dataGridView2.Rows[i].Cells[j].Value = matrix[i, j];
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s = "The quick  brown   fox    jumps over the  lazy dog";
+            label4.Text = ("Исходная строка: " + s);
+            int maxSpaces = 0;
+            int currentSpaces = 0;
+            foreach (char c in s)
+            {
+                if (c == ' ')
+                {
+                    currentSpaces++;
+                    if (currentSpaces > maxSpaces)
+                    {
+                        maxSpaces = currentSpaces;
+                    }
+                }
+                else
+                {
+                    currentSpaces = 0;
+                }
+            }
+            label5.Text = ($"Максимальное количество идущих подряд пробелов: {maxSpaces}");
+            int eCount = 0;
+            foreach (char c in s)
+            {
+                if (c == 'e')
+                {
+                    eCount++;
+                    if (eCount >= 5)
+                    {
+                        label6.Text = ("В строке есть пять идущих подряд букв 'e'");
+                        break;
+                    }
+                }
+                else
+                {
+                    eCount = 0;
+                }
+            }
+            if (eCount < 5)
+            {
+                label6.Text = ("В строке нет пяти идущих подряд букв 'e'");
+            }
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            List<(string, double, string)> substances = new List<(string, double, string)> {
+            ("Алмаз", 3.52, "Изолятор"),
+            ("Кремний", 2.33, "Полупроводник"),
+            ("Золото", 19.3, "Проводник"),
+            ("Серебро", 10.5, "Проводник"),
+            ("Кварц", 2.65, "Изолятор"),
+            ("Керамика", 2.5, "Изолятор"),
+            ("Силикон", 2.33, "Полупроводник"),
+            ("Медь", 8.96, "Проводник"),
+            ("Германий", 5.32, "Полупроводник"),
+            ("Олово", 7.29, "Проводник")
+        };
+            // a) Находим удельные веса и названия всех полупроводников
+            var semiconductors = substances.Where(s => s.Item3 == "Полупроводник")
+                                           .Select(s => (s.Item1, s.Item2))
+                                           .ToList();
+            label7.Text += ("Удельные веса и названия всех полупроводников: ");
+            foreach (var semiconductor in semiconductors)
+            {
+                label7.Text += ($"{semiconductor.Item1}: {semiconductor.Item2}") + ' ';
+            }
+
+            // б) Выбираем данные о проводниках и упорядочиваем их по убыванию удельных весов
+            var conductors = substances.Where(s => s.Item3 == "Проводник")
+                                       .OrderByDescending(s => s.Item2)
+                                       .Select(s => (s.Item1, s.Item2))
+                                       .ToList();
+            label8.Text += ("\nДанные о проводниках, упорядоченные по убыванию удельных весов: ");
+            foreach (var conductor in conductors)
+            {
+                label8.Text += ($"{conductor.Item1}: {conductor.Item2}" + ' ');
+            }
+
+        }
+    }
+}
